e9d3c6991bcfb8f79891f4e193f7aac91b831b04
Alex Koval <al3xander.koval@gmail.com>
2025-05-17 20:55:21 +0200

    feat: add realtime stats methods

--- lib/taboola_api/resources/reportings.rb
+++ lib/taboola_api/resources/reportings.rb
@@ -6,6 +6,20 @@ module TaboolaApi
         response = client.request(:get, "#{account_id}/reports/top-campaign-content/dimensions/item_breakdown", params)
         response.body
       end
+
+      def realtime_campaign_report(account_id:, dimension:, params: {})
+        raise ArgumentError, "start_date and end_date params are required" unless params[:start_date] && params[:end_date]
+
+        response = client.request(:get, "#{account_id}/reports/reports/realtime-campaign-summary/dimensions/#{dimension}", params)
+        response.body
+      end
+
+      def realtime_ads_report(account_id:, dimension:, params: {})
+        raise ArgumentError, "start_date and end_date params are required" unless params[:start_date] && params[:end_date]
+
+        response = client.request(:get, "#{account_id}/reports/reports/realtime-top-campaign-content/dimensions/#{dimension}", params)
+        response.body
+      end
     end
   end
 end
